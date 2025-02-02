081ed20090713dcb04ea40412d6cc07e5f969965
Damaroo <levkichigin@yandex.ru>
2025-02-02 16:22:36 +0300

    blog page fix

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,9 @@
 'use client'
 import { useParams } from 'next/navigation'
 import { useState } from 'react'
-export default async function Page() {
+export default function Page() {
   const [post, setPost] = useState({})
   const params = useParams()
-
-  // const resp = await fetch(`https://api.vercel.app/blog/${params?.id}`)
-  // const data = await resp.json()
-  // setPost(data)
 
   return <h1>{JSON.stringify(post, null, 2)}</h1>
 }
