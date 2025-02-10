8ae799f2d71f9fdf3553a377051ab29fc0490b24
Damaroo <levkichigin@yandex.ru>
2025-02-11 01:48:27 +0300

    some fixes

--- src/app/components/main/gsapCircles/index.tsx
+++ src/app/components/main/gsapCircles/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { memo, useRef } from 'react'
+import { useRef } from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
